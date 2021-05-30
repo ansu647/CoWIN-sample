8e23d3970c6a4757675095f99d1f19c556a85451
Ansu Sinha <84193543+ansu647@users.noreply.github.com>
2021-05-30 22:57:44 +0530

    Update Server.py

--- API/Server.py
+++ API/Server.py
@@ -11,7 +11,7 @@ def index():
 def i():
     y = request.form["district_id"]
     today = date.today().strftime("%d-%m-%Y")
-    url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=140&date=06-05-2021"
+    url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={}&date={}".format(y,today)
     payload={}
     headers = {
         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
@@ -22,6 +22,7 @@ def i():
     }
     response = requests.request("GET", url, headers=headers, data=payload)
     res= response.json()
+    print(res)
     for session in res['sessions']:
         if session['available_capacity'] > 0:
             w[session['center_id']]=session['address']
